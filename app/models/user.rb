54a900214e8780d37b9d7faa649e0411d304f0a2
Michael Chinaloy <michael.chinaloy@gmail.com>
2022-02-08 09:57:34 +0000

    - chapter 10

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
 
-    attr_accessor :remember_token
+    attr_accessor :remember_token, :activation_token
 
-    before_save { self.email = email.downcase }
+    before_save :downcase_email
+    before_create :create_activation_digest
 
     validates :name, presence: true, length: { maximum: 50 }
     validates :password, length: { minimum: 6 }, allow_blank: true
@@ -10,6 +11,15 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
     has_secure_password
+
+    def downcase_email
+        self.email = email.downcase
+    end
+
+    def create_activation_digest
+        self.activation_token = User.new_token
+        self.activation_digest = User.digest(activation_token)
+    end
 
     def User.digest(string)
         cost = nil
@@ -30,9 +40,10 @@ class User < ApplicationRecord
         update_attribute(:remember_digest, User.digest(remember_token))
     end
 
-    def authenticated?(remember_token)
-        return false if remember_digest.nil?
-        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    def authenticated?(attribute, token)
+        digest = send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
     end
 
     def forget
